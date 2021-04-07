01ded86fb4526d715e1df1bd9c8dcddc7c701cb7
yyh834771838 <834771838@qq.com>
2021-04-07 23:21:42 +0800

    Test

---
+++ Test/bean/HeroBeanTest.java
@@ -0,0 +1,30 @@
+package bean;
+
+import static org.junit.Assert.*;
+
+public class HeroBeanTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getTime() {
+    }
+
+    @org.junit.Test
+    public void setTime() {
+    }
+
+    @org.junit.Test
+    public void getName() {
+    }
+
+    @org.junit.Test
+    public void setName() {
+    }
+}
