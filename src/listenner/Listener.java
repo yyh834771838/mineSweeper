cf8e1f76890f7c43b3f3184d0cf0a88b4578c3a7
Youzebin <u201817114@hust.edu.cn>
2021-04-14 21:31:57 +0800

    'listener添加一点注释'

--- src/listenner/Listener.java
+++ src/listenner/Listener.java
@@ -49,17 +49,17 @@ public class Listener implements MouseListener {
 
 		if (e.getModifiersEx() == InputEvent.BUTTON1_DOWN_MASK
 				+ InputEvent.BUTTON3_DOWN_MASK) {
-			isDoublePress = true;
+			isDoublePress = true;//双击
 			doublePress(row, col);
 
 		} else if (e.getModifiers() == InputEvent.BUTTON1_MASK
 				&& mineLable.isFlagTag() == false) {
 			if (mineLable.isExpendTag() == false) {
-				mineLable.setIcon(StaticTool.icon0);
+				mineLable.setIcon(StaticTool.icon0); //点开没有空格的
 
 			}
 			mainFrame.getFaceJPanel().getLabelFace()
-					.setIcon(StaticTool.clickIcon);
+					.setIcon(StaticTool.clickIcon); //点击时表情变化
 		} else if (e.getModifiers() == InputEvent.BUTTON3_MASK
 				&& mineLable.isExpendTag() == false) {
 			if (mineLable.getRightClickCount() == 0) {
@@ -67,17 +67,17 @@ public class Listener implements MouseListener {
 				mineLable.setRightClickCount(1);
 				mineLable.setFlagTag(true);
 				StaticTool.bombCount--;
-				mainFrame.getFaceJPanel().setNumber(StaticTool.bombCount);
+				mainFrame.getFaceJPanel().setNumber(StaticTool.bombCount);//右键时将雷数减一
 
 			} else if (mineLable.getRightClickCount() == 1) {
 				mineLable.setIcon(StaticTool.askIcon);
 				mineLable.setRightClickCount(2);
 				mineLable.setFlagTag(false);
 				StaticTool.bombCount++;
-				mainFrame.getFaceJPanel().setNumber(StaticTool.bombCount);
+				mainFrame.getFaceJPanel().setNumber(StaticTool.bombCount);//右键第二下将旗子变为问号
 
 			} else {
-				mineLable.setIcon(StaticTool.iconBlank);
+				mineLable.setIcon(StaticTool.iconBlank);//第三下设定为初始状态
 				mineLable.setRightClickCount(0);
 			}
 
@@ -138,7 +138,7 @@ public class Listener implements MouseListener {
 		isWin();
 	}
 
-	private void bombAction(int row, int col) {
+	private void bombAction(int row, int col) {//点击到雷之后
 
 		for (int i = 0; i < mineLable.length; i++) {
 			for (int j = 0; j < mineLable[i].length; j++) {
@@ -149,18 +149,19 @@ public class Listener implements MouseListener {
 				} else {
 					if (mineLable[i][j].isFlagTag()) {
 						mineLable[i][j].setIcon(StaticTool.errorBombIcon);
+						//插旗子点击了之后会变成errorBombIcon
 					}
 				}
 			}
 
 		}
 
-		mainFrame.getTimer().stop();
+		mainFrame.getTimer().stop(); //时间停止
 
 		for (int i = 0; i < mineLable.length; i++) {
 			for (int j = 0; j < mineLable[i].length; j++) {
 				mineLable[i][j].removeMouseListener(this);
-
+				//不能再点击  点击失效
 			}
 		}
 
@@ -180,7 +181,7 @@ public class Listener implements MouseListener {
 						mineLable.length - 1, x + 1); i++) {
 					for (int j = Math.max(0, y - 1); j <= Math.min(
 							mineLable[x].length - 1, y + 1); j++) {
-						expand(i, j);
+						expand(i, j); //深度优先搜索 不断扩展周围为没有雷的 并且显示图标
 
 					}
 
@@ -188,7 +189,7 @@ public class Listener implements MouseListener {
 
 			} else {
 
-				mineLable[x][y].setIcon(StaticTool.num[count]);
+				mineLable[x][y].setIcon(StaticTool.num[count]);//假如周围有雷 那么不扩展 并且设置扩展tag为true
 				mineLable[x][y].setExpendTag(true);
 
 			}
@@ -209,8 +210,7 @@ public class Listener implements MouseListener {
 						mineLable[x][y].setIcon(StaticTool.askIcon);
 					} else {
 						mineLable[x][y].setIcon(StaticTool.iconBlank);
-
-					}
+					}//设定图标 假如点击第二下 那么问号 右键第三下恢复
 				}
 			}
 		}
@@ -225,7 +225,7 @@ public class Listener implements MouseListener {
 			for (int y = Math.max(0, j - 1); y <= Math.min(
 					StaticTool.allcol - 1, j + 1); y++) {
 				if (mineLable[x][y].isFlagTag()) {
-					flagCount++;
+					flagCount++;//判断周围的旗子数量是不是真实的周围数
 				}
 			}
 		}
@@ -235,7 +235,7 @@ public class Listener implements MouseListener {
 		return false;
 	}
 
-	private void doublePress(int i, int j) {
+	private void doublePress(int i, int j) {//方法有点奇怪 右键点击过一次和点击过两次？
 		for (int x = Math.max(0, i - 1); x <= Math.min(StaticTool.allrow - 1,
 				i + 1); x++) {
 			for (int y = Math.max(0, j - 1); y <= Math.min(
@@ -245,7 +245,6 @@ public class Listener implements MouseListener {
 					int rightClickCount = mineLable[x][y].getRightClickCount();
 					if (rightClickCount == 1) {
 						mineLable[x][y].setIcon(StaticTool.askPressIcon);
-
 					} else {
 						mineLable[x][y].setIcon(StaticTool.icon0);
 
@@ -262,13 +261,13 @@ public class Listener implements MouseListener {
 					StaticTool.allcol - 1, j + 1); y++) {
 				if (mineLable[x][y].isMineTag()) {
 					if (mineLable[x][y].isFlagTag() == false) {
-						bombAction(x, y);
+						bombAction(x, y);//是雷并且没有设定旗子
 
 					}
 				} else {
 
 					if (mineLable[x][y].isFlagTag() == false) {
-						expand(x, y);
+						expand(x, y);//不是雷 没有设定旗子  打开该雷区
 					}
 
 				}
@@ -286,7 +285,7 @@ public class Listener implements MouseListener {
 		for (int i = 0; i < mineLable.length; i++) {
 			for (int j = 0; j < mineLable[i].length; j++) {
 				if (mineLable[i][j].isExpendTag()) {
-					expendCount++;
+					expendCount++; //是否已经被打开 就是显示数字的地方
 				}
 
 			}
@@ -297,16 +296,16 @@ public class Listener implements MouseListener {
 				for (int j = 0; j < mineLable[i].length; j++) {
 					if (mineLable[i][j].isMineTag()
 							&& mineLable[i][j].isFlagTag() == false) {
-						mineLable[i][j].setIcon(StaticTool.flagIcon);
-						mineLable[i][j].setFlagTag(true);
+						mineLable[i][j].setIcon(StaticTool.flagIcon); //假如没有设置旗子，那么自动设置旗子
+						mineLable[i][j].setFlagTag(true);//并且将flagTag设置为true
 					}
 
 				}
 
 			}
 
-			mainFrame.getFaceJPanel().setNumber(0);
-			mainFrame.getTimer().stop();
+			mainFrame.getFaceJPanel().setNumber(0); //设置剩余雷数为0
+			mainFrame.getTimer().stop();//胜利之后不能再点击雷盘
 			for (int i = 0; i < mineLable.length; i++) {
 				for (int j = 0; j < mineLable[i].length; j++) {
 					mineLable[i][j].removeMouseListener(this);
@@ -315,7 +314,7 @@ public class Listener implements MouseListener {
 			}
 
 			mainFrame.getFaceJPanel().getLabelFace()
-					.setIcon(StaticTool.winFaceIcon);
+					.setIcon(StaticTool.winFaceIcon); //表情变化 胜利图标
 			int level = StaticTool.getLevel();
 			if (level != 0) {
 				if (level == 1) {
