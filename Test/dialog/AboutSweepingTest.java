e0ae652d9807d36a600a63172dbe65f43efd697c
yyh834771838 <834771838@qq.com>
2021-04-17 15:56:13 +0800

    "yyh first test"

---
+++ Test/dialog/AboutSweepingTest.java
@@ -0,0 +1,23 @@
+package dialog;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AboutSweepingTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getPanel() {
+
+    }
+}
